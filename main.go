a2a44b2c8fc659a9e654c5117052a95dd24b4743
Coding-Seal <rodion2023@mail.ru>
2024-03-28 18:26:35 +0300

    Updated cli to support default dictionary

--- main.go
+++ main.go
@@ -10,24 +10,26 @@ import (
 func main() {
 	var input string
 	flag.StringVar(&input, "s", "", "String to stem")
-	var stopWordsFile string
-	flag.StringVar(&stopWordsFile, "file", "", "File with stop words")
+	var stopWordsFileName string
+	flag.StringVar(&stopWordsFileName, "file", "", "File with stop words")
 	flag.Parse()
 	if input == "" {
 		fmt.Println("Provide string to stem using -s flag")
 		os.Exit(1)
 	}
-	if stopWordsFile == "" {
-		fmt.Println("Provide file with stop words using --file flag")
-		os.Exit(1)
+	// Handle file with stopWords
+	var stopWords map[string]struct{}
+	if stopWordsFileName != "" {
+		stopWordsFile, err := os.Open(stopWordsFileName)
+		if err != nil {
+			fmt.Printf("Could not open file \"%s\"\n", stopWordsFileName)
+			os.Exit(1)
+		}
+		stopWords = ParseStopWords(stopWordsFile)
 	}
-	words := strings.Fields(strings.ToLower(input))
-	stopWords, err := os.Open(stopWordsFile)
-	if err != nil {
-		fmt.Printf("Could not open file \"%s\"\n", stopWordsFile)
-		os.Exit(1)
-	}
-	stemmer := NewStemmer(ParseStopWords(stopWords))
+
+	words := strings.Fields(input)
+	stemmer := NewStemmer(stopWords)
 	stemmed := stemmer.Stem(words)
 	fmt.Println(strings.Join(stemmed, " "))
 }
