29b7f260b32fc2d184f43b1b90e84fcf98ad9b11
Coding-Seal <rodion2023@mail.ru>
2024-03-28 18:06:43 +0300

    Added a way to easily change stop words

--- main.go
+++ main.go
@@ -10,12 +10,24 @@ import (
 func main() {
 	var input string
 	flag.StringVar(&input, "s", "", "String to stem")
+	var stopWordsFile string
+	flag.StringVar(&stopWordsFile, "file", "", "File with stop words")
 	flag.Parse()
 	if input == "" {
 		fmt.Println("Provide string to stem using -s flag")
 		os.Exit(1)
 	}
+	if stopWordsFile == "" {
+		fmt.Println("Provide file with stop words using --file flag")
+		os.Exit(1)
+	}
 	words := strings.Fields(strings.ToLower(input))
-	stemmed := Stem(words)
+	stopWords, err := os.Open(stopWordsFile)
+	if err != nil {
+		fmt.Printf("Could not open file \"%s\"\n", stopWordsFile)
+		os.Exit(1)
+	}
+	stemmer := NewStemmer(ParseStopWords(stopWords))
+	stemmed := stemmer.Stem(words)
 	fmt.Println(strings.Join(stemmed, " "))
 }
--- stemmer.go
+++ stemmer.go
@@ -1,44 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"github.com/kljensen/snowball"
+	"io"
 )
 
-// isStopWord is function, that check if word is not significant
-// list of words from https://www.ranks.nl/stopwords
-func isStopWord(word string) bool {
-	switch word {
-	case "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any",
-		"are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between",
-		"both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does",
-		"doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had",
-		"hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her",
-		"here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd",
-		"i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself",
-		"let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off",
-		"on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over",
-		"own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
-		"some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves",
-		"then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this",
-		"those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd",
-		"we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where",
-		"where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would",
-		"wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves":
-		return true
-	default:
-		return false
-
-	}
+type Stemmer struct {
+	stopWords map[string]struct{}
 }
-func Stem(words []string) []string {
-	stemmed := make([]string, 0, len(words)/2)
+
+func NewStemmer(stopWords map[string]struct{}) *Stemmer {
+	return &Stemmer{stopWords: stopWords}
+}
+
+func ParseStopWords(reader io.Reader) map[string]struct{} {
+	stopWords := make(map[string]struct{})
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		word := scanner.Text()
+		if word != "" {
+			stopWords[word] = struct{}{}
+		}
+	}
+	return stopWords
+}
+
+// isStopWord is method, that check if word is not significant
+func (s *Stemmer) isStopWord(word string) bool {
+	_, ok := s.stopWords[word]
+	return ok
+}
+func (s *Stemmer) Stem(words []string) []string {
+	// using map to avoid duplicates
+	stemmed := make(map[string]struct{})
 	for _, word := range words {
-		if isStopWord(word) || word == "" {
+		if s.isStopWord(word) || word == "" {
 			continue
 		} else {
 			word, _ = snowball.Stem(word, "english", false)
-			stemmed = append(stemmed, word)
+			stemmed[word] = struct{}{}
 		}
 	}
-	return stemmed
+	// transform map into slice
+	keys := make([]string, 0, len(stemmed))
+	for k := range stemmed {
+		keys = append(keys, k)
+	}
+	return keys
 }
