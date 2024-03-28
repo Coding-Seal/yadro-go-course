870d12dbe15d34fc69f17897e999238bf5b979e3
Coding-Seal <rodion2023@mail.ru>
2024-03-28 17:21:33 +0300

    Added check for arguments

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -10,6 +11,10 @@ func main() {
 	var input string
 	flag.StringVar(&input, "s", "", "String to stem")
 	flag.Parse()
+	if input == "" {
+		fmt.Println("Provide string to stem using -s flag")
+		os.Exit(1)
+	}
 	words := strings.Fields(strings.ToLower(input))
 	stemmed := Stem(words)
 	fmt.Println(strings.Join(stemmed, " "))
