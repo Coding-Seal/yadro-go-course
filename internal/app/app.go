76f7aefddbd0b317b264c0bc709910fb730bc0e0
Coding-Seal <rodion2023@mail.ru>
2024-04-06 22:40:59 +0300

    Implemented App

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"maps"
 	"yadro-go-course/pkg/comic"
 	"yadro-go-course/pkg/database"
@@ -15,25 +18,74 @@ type App struct {
 	comics  map[int]*comic.Comic
 }
 
+func NewApp(source string, rw io.ReadWriter, stopWords map[string]struct{}) *App {
+	return &App{
+		fetcher: xkcd.NewFetcher(source),
+		stemmer: words.NewStemmer(stopWords),
+		db:      database.NewJsonDB(rw),
+		comics:  make(map[int]*comic.Comic),
+	}
+}
+
 func (a *App) LoadComics() {
 	maps.Copy(a.comics, a.db.Read())
 }
 func (a *App) SaveComics() {
 	a.db.Save(a.comics)
 }
-func (a *App) FetchRemainingComics(lastID int) {
+func (a *App) FetchRemainingComics(lastID int, ctx context.Context) {
 	var missingComics []int
+
 	for id := 1; id <= lastID; id++ {
 		if _, ok := a.comics[id]; !ok {
 			missingComics = append(missingComics, id)
 		}
 	}
-}
-func (a *App) GetLastComicID() int {
 
+	for _, fetchedComic := range a.fetcher.GetComics(ctx, missingComics) {
+		if fetchedComic != nil {
+			a.comics[fetchedComic.ID] = a.toComic(fetchedComic)
+		}
+	}
 }
-func (a *App) RefreshDB(lastID int) int {
+
+func (a *App) FetchLastComicID(ctx context.Context) (int, error) {
+	return a.fetcher.GetLastID(ctx)
 }
+func (a *App) FetchAll(lastID int, ctx context.Context) {
+	for _, fetchedComic := range a.fetcher.GetALLComics(ctx, lastID) {
+		if fetchedComic != nil {
+			a.comics[fetchedComic.ID] = a.toComic(fetchedComic)
+		}
+	}
+}
+
 func (a *App) PrintComics(num int) {
+	i := 1
 
+	for _, c := range a.comics {
+		fmt.Printf("ID=%d ImgURl=%s Keywords=%v\n", c.ID, c.ImgURL, c.Keywords)
+
+		i++
+
+		if i >= num {
+			break
+		}
+	}
+}
+func (a *App) PrintAllComics() {
+	for _, c := range a.comics {
+		fmt.Printf("ID=%d ImgURl=%s Keywords=%v\n", c.ID, c.ImgURL, c.Keywords)
+	}
+}
+func (a *App) toComic(f *xkcd.FetchedComic) *comic.Comic {
+	if f == nil {
+		return nil
+	}
+
+	return &comic.Comic{
+		ID:       f.ID,
+		ImgURL:   f.ImgURL,
+		Keywords: a.stemmer.Stem(words.ParsePhrase(f.Title + " " + f.AltTranscription + " " + f.Transcription)),
+	}
 }
