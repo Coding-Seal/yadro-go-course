fa29ff7e838228d918ed16ddb4b68dd15957ed36
Coding-Seal <rodion2023@mail.ru>
2024-04-23 21:21:55 +0300

    Added a way to sort comics

---
+++ internal/app/search.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"yadro-go-course/internal/comic"
+	"yadro-go-course/pkg/words"
+	"yadro-go-course/pkg/xkcd"
+)
+
+func giveWordScore(inPhrase, inKeywords int) int {
+	return min(inPhrase*inKeywords, 3)
+}
+
+func (a *App) toComic(c *xkcd.Comic) *comic.Comic {
+	return &comic.Comic{
+		ID:       c.ID,
+		Title:    c.Title,
+		Date:     c.Date,
+		ImgURL:   c.ImgURL,
+		Keywords: a.stemmer.Stem(words.ParsePhrase(c.Title + " " + c.AltTranscription + " " + c.Transcription + " " + c.News)),
+	}
+}
+func (a *App) SearchComics(searchPhrase string) map[*comic.Comic]int {
+	searchWords := a.stemmer.Stem(words.ParsePhrase(searchPhrase))
+	res := make(map[*comic.Comic]int, 0)
+
+	for _, c := range a.comics {
+		score := 0
+		for word, numWords := range searchWords {
+			score += giveWordScore(numWords, c.Keywords[word])
+		}
+
+		if score > 0 {
+			res[c] = score
+		}
+	}
+
+	return res
+}
+func (a *App) BuildIndex() {
+	a.index = make(map[string][]int, len(a.comics)*10)
+	for id, c := range a.comics {
+		for word := range c.Keywords {
+			a.index[word] = append(a.index[word], id)
+		}
+	}
+}
+func (a *App) SearchIndex(searchPhrase string) map[*comic.Comic]int {
+	searchWords := a.stemmer.Stem(words.ParsePhrase(searchPhrase))
+	foundComics := make(map[*comic.Comic]int)
+
+	for word, num := range searchWords {
+		for _, id := range a.index[word] {
+			foundComics[a.comics[id]] += giveWordScore(num, a.comics[id].Keywords[word])
+		}
+	}
+
+	return foundComics
+}
