fc7ec181f06b2ac0d558e1f0e8cf5f90259083b7
Coding-Seal <rodion2023@mail.ru>
2024-05-06 15:48:11 +0300

    Added map

--- internal/adapters/repos/comic/jsonStore.go
+++ internal/adapters/repos/comic/jsonStore.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"os"
+	"sync"
 	"yadro-go-course/internal/core/models"
 	"yadro-go-course/internal/core/ports"
 	"yadro-go-course/pkg/jsonl"
@@ -14,32 +15,53 @@ var ErrInternal = errors.New("internal error")
 
 type JsonRepo struct {
 	file *os.File
+	m    map[int]models.Comic
+	mu   sync.RWMutex
 }
 
 var _ ports.ComicsRepo = (*JsonRepo)(nil)
 
 func NewJsonDB(file *os.File) *JsonRepo {
-	return &JsonRepo{
+	r := &JsonRepo{
 		file: file,
+		m:    make(map[int]models.Comic),
 	}
+	sc := jsonl.NewScanner(file)
+
+	for sc.Scan() {
+		var comic models.Comic
+		err := sc.Json(&comic)
+
+		if err != nil {
+			break
+		}
+
+		r.m[comic.ID] = comic
+	}
+
+	return r
 }
 
 func (db *JsonRepo) Comic(ctx context.Context, id int) (models.Comic, error) {
-	sc := jsonl.NewScanner(db.file)
-	var readComic models.Comic
-	for sc.Scan() {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
 
-		if err := sc.Json(&readComic); err != nil {
-			return readComic, errors.Join(ErrInternal, err)
-		}
-		if readComic.ID == id {
-			return readComic, nil
-		}
+	if comic, ok := db.m[id]; ok {
+		return comic, nil
 	}
 
-	return readComic, ErrNotFound
+	return models.Comic{}, ErrNotFound
 }
 func (db *JsonRepo) Store(ctx context.Context, comic models.Comic) error {
+	db.mu.RLock()
+	if _, ok := db.m[comic.ID]; ok {
+		db.mu.RUnlock()
+		return nil
+	}
+	db.mu.RUnlock()
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
 	wr := jsonl.NewWriter(db.file)
 
 	if err := wr.WriteJson(comic); err != nil {
@@ -49,5 +71,6 @@ func (db *JsonRepo) Store(ctx context.Context, comic models.Comic) error {
 	if err := wr.Flush(); err != nil {
 		return errors.Join(ErrInternal, err)
 	}
+
 	return nil
 }
