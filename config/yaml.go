2eaeffa135b65920ed57a773934628f1a65e039e
Coding-Seal <rodion2023@mail.ru>
2024-05-06 15:46:15 +0300

    Changed config

---
+++ config/yaml.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type (
+	Config struct {
+		DB      `yaml:"db"`
+		Fetcher `yaml:"fetcher"`
+		Server  `yaml:"server"`
+		Logger  `yaml:"logger"`
+		Search  `yaml:"search"`
+	}
+	Logger struct {
+		Type  string `yaml:"type"`
+		Level string `yaml:"level"`
+	}
+	DB struct {
+		Type string `yaml:"type"`
+		Url  string `yaml:"url"`
+	}
+	Search struct {
+		StopWordsFile string `yaml:"stop_words_file"`
+	}
+	Fetcher struct {
+		SourceURL string `yaml:"source_url"`
+		Parallel  int    `yaml:"parallel"`
+	}
+
+	Server struct {
+		Port int `yaml:"port"`
+	}
+)
+
+func NewConfig(configPath string) (*Config, error) {
+	config := &Config{
+		DB: DB{
+			Type: "json",
+			Url:  "db.jsonl",
+		},
+		Server: Server{
+			Port: 8080,
+		},
+		Logger: Logger{
+			Type:  "json",
+			Level: "debug",
+		},
+		Search: Search{
+			StopWordsFile: "stopwords.txt",
+		},
+		Fetcher: Fetcher{
+			SourceURL: "https://xkcd.com",
+			Parallel:  100,
+		},
+	}
+
+	file, err := os.Open(configPath)
+
+	if err != nil {
+		return config, fmt.Errorf("loading config: %w", err)
+	}
+
+	defer file.Close()
+	d := yaml.NewDecoder(file)
+	err = d.Decode(&config)
+
+	if err != nil {
+		return config, fmt.Errorf("parsing config: %w", err)
+	}
+
+	return config, nil
+}
